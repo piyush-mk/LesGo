f3fb07c3f5e240bf1b6cada68e6d10ed54810f25
Piyush Mohapatra <piyush.ofcl@gmail.com>
2023-01-24 01:43:40 +0530

    distance added

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
+# streamlit app to create a travel itinerary based on location and budget constraints
+# data
+def load_data():
+    df = pd.read_csv('joined_data.csv')
+    return df
+
+data=load_data()
+
+# ml model to recommend hotels based on location and budget and return the top 5 hotels and restaurants in the area with their ratings
+def recommend_hotels(location, budget):
+    
