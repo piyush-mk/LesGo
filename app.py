fe5325eb5d88c4828b4a0fc12b0233a77c6d7b54
Piyush Mohapatra <piyush.ofcl@gmail.com>
2023-01-24 03:34:13 +0530

    minor change

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from PIL import Image
-im = Image.open("asset/favicon.png")
-st.set_page_config(page_title="LesGo | Travel itinerary maker", page_icon="im", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="LesGo | Travel itinerary maker", page_icon="🏝️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
