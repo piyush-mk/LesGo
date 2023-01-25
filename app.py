df1a7c58d1d9dc2f8efcca46a8bdd36a038c5ae0
Piyush Mohapatra <piyush.ofcl@gmail.com>
2023-01-25 17:19:58 +0530

    checkpoint

--- app.py
+++ app.py
@@ -26,10 +26,11 @@ def hotel_knn_model(df, city, h_budget):
     df = df[['H_name', 'H_price', 'H_rating']]
     df = df.drop_duplicates()
     df = df.reset_index(drop=True)
-    df = df.iloc[:5]
+    df = df.iloc[:3]
     return df
     
-
+#    
+    
 def page():
     st.markdown("""
     <style>
@@ -41,12 +42,13 @@ def page():
     #catch nan error for the form
     try:
         with st.form(key='myform'):
+            source_city=st.selectbox('Select your source city',df['location'].sort_values().unique())
             placetype=st.selectbox('Select the type of attractions you want to see',df['P_type'].sort_values().unique())
             budget=st.slider('Select your Total budget', 0, 150000, 500)
             submit_button=st.form_submit_button(label='Submit')
             days=st.slider('Select the duration of travel', 0, 15, 5)
         if submit_button:
-            flightcost=budget*0.4
+            flightcost=budget*0.3
             r_budget=budget*0.1
             h_budget=(budget-flightcost-r_budget)/days
             
@@ -54,14 +56,14 @@ def page():
             #display the city name in stylized format using markdown big font and bold text with red color
             city=get_city(df, placetype)
             st.markdown(f"""<span style="font-size: 40px; font-weight: bold; color: #eb4034;">{city}</span>""", unsafe_allow_html=True)
-            st.write("### Your budget has been divided as follows:")
-            st.markdown(f"""<span style="font-size: 20px; font-weight: bold; color: #ffffff;">Flight Budget: </span>"""f"""<span style="font-size: 20px; font-weight: bold; color: #ffd000;">Rs. {flightcost}</span>""", unsafe_allow_html=True)
-            st.markdown(f"""<span style="font-size: 20px; font-weight: bold; color: #ffffff;">Hotel Budget: </span>"""f"""<span style="font-size: 20px; font-weight: bold; color: #ffd000;">  Rs. {h_budget}</span>""", unsafe_allow_html=True)
-            st.markdown(f"""<span style="font-size: 20px; font-weight: bold; color: #ffffff;">Food Budget: </span>"""f"""<span style="font-size: 20px; font-weight: bold; color: #ffd000;"> Rs. {r_budget}</span>""", unsafe_allow_html=True)
+            st.write("### Your budget has been divided as follows -")
+            st.markdown(f"""<span style="font-size: 20px; font-weight: bold; color: #ffffff;">Travel cost from {source_city} to {city} is (includes the flight/train and cab): </span>"""f"""<span style="font-size: 20px; font-weight: bold; color: #ffd000;"><br>Rs. {flightcost}</span>""", unsafe_allow_html=True)
+            st.markdown(f"""<span style="font-size: 20px; font-weight: bold; color: #ffffff;">Hotel Budget: </span>"""f"""<span style="font-size: 20px; font-weight: bold; color: #ffd000;"><br>Rs. {h_budget}</span>""", unsafe_allow_html=True)
+            st.markdown(f"""<span style="font-size: 20px; font-weight: bold; color: #ffffff;">Food Budget: </span>"""f"""<span style="font-size: 20px; font-weight: bold; color: #ffd000;"><br>Rs. {r_budget}</span>""", unsafe_allow_html=True)
             
             
             
-            st.write("### Here are the top 5 hotels we recommend:")
+            st.write("### Here are the top 3 hotels we recommend:")
             df2=hotel_knn_model(df, city, h_budget)
             df2.columns=['Hotel Name', 'Price(/day)', 'Rating']
             st.table(df2)
